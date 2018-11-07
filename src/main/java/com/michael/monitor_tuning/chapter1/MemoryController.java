010b0b5708671cc3a7d27d5540a0fb23cf03a667
hengli <12345678>
2018-11-07 11:44:25 +0800

    first commit

---
+++ src/main/java/com/michael/monitor_tuning/chapter1/MemoryController.java
@@ -0,0 +1,31 @@
+package com.michael.monitor_tuning.chapter1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @auther hengli
+ * @date 2018/11/7 10:50
+ */
+@RestController
+public class MemoryController {
+
+    private List<User> userList = new ArrayList<>();
+
+    /**
+     * -Xmx32M -Xms32M 设置堆栈内存.
+     * @return
+     */
+    @GetMapping("/heap")
+    public String heap() {
+        while (true) {
+            int i = 0;
+            userList.add(new User(i++, UUID.randomUUID().toString()));
+        }
+    }
+}
---
+++ src/main/java/com/michael/monitor_tuning/chapter1/User.java
@@ -0,0 +1,39 @@
+package com.michael.monitor_tuning.chapter1;
+
+/**
+ * @auther hengli
+ * @date 2018/11/7 10:51
+ */
+public class User {
+    private int id;
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
